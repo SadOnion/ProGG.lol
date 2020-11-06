111b9afbde47b54ce01ebda0ecf9e99a00926501
rdk31 <rdk31@protonmail.com>
2020-11-07 00:50:19 +0100

    dont divide by 0

--- progglol/analyses/winRatio.py
+++ progglol/analyses/winRatio.py
@@ -9,4 +9,7 @@ class WinRatio(Analysis):
             winCount += leagueEntry.wins
             loseCount += leagueEntry.losses
 
+        if winCount + loseCount == 0:
+            return 0
+
         return winCount / (winCount + loseCount)
